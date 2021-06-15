b58acbdb74a968dee92a899f25f6d31cd0821e2e
zymousai <zymousai@163.com>
2021-06-15 23:18:59 +0800

    定稿使用selenium并且完成下拉

--- core/spider.py
+++ core/spider.py
@@ -2,8 +2,10 @@
 spider
 """
 import random
+import time
 
-import requests
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 
 class YSpider(object):
@@ -21,44 +23,75 @@ class YSpider(object):
     ]
 
     def __init__(self, keyword):
-        # self.__url = f'https://www.youtube.com/results?search_query={keyword}'
-        self.__url = 'https://www.youtube.com/youtubei/v1/search?key=AIzaSyAO_FJ2SlqU8Q4STEHLGCilw_Y9_11qcW8'
+        self.__url = f'https://www.youtube.com/results?search_query={keyword}'
 
         self.__headers = {
             'user-agent': random.choice(self.UA_LIST)
         }
 
-        self.__proxies = {
-            "http": "socks5://127.0.0.1:5000",
-            "https": "socks5://127.0.0.1:5000"
-        }
+        self.__chrome_path = './tools/Chrome/chrome.exe'
+
+        self.__chrome_driver_path = './tools/Chrome/chromedriver.exe'
+
+        self.__js_path = './tools/Chrome/stealth.min.js'
+
+        self.__webrtc_path = './tools/Chrome/webrtc.crx'
+
+        self.__opt = None
+
+    def __create_opt(self):
+        self.__opt = webdriver.ChromeOptions()
+
+        # https忽略证书错误
+        self.__opt.add_argument('--ignore-certificate-errors')
+        # 指定ua
+        self.__opt.add_argument('--user-agent=%s' % random.choice(self.UA_LIST))
+        # 添加webrtc
+        self.__opt.add_extension(self.__webrtc_path)
+        # 不显示测试软件正在控制的消息
+        self.__opt.add_experimental_option('excludeSwitches', ['enable-automation'])
+        self.__opt.add_experimental_option("detach", True)
+        # 二进制方式指定浏览器路径
+        self.__opt.binary_location = self.__chrome_path
+
+    def __create_driver(self):
+        driver = webdriver.Chrome(executable_path=self.__chrome_driver_path, chrome_options=self.__opt)
+
+        with open(self.__js_path) as f:
+            js = f.read()
+        driver.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
+            "source": js
+        })
+
+        return driver
+
+    @staticmethod
+    def __down(body):
+        for x in range(20):
+            body.send_keys(Keys.PAGE_DOWN)
+            time.sleep(random.uniform(0.3, 0.5))
+
+    def __open_browser(self):
+        self.__create_opt()
+        driver = self.__create_driver()
+        driver.get(self.__url)
+
+        body = driver.find_element_by_tag_name('body')
+
+        time.sleep(5)
+        while True:
+            contents = driver.find_element_by_id("contents")
+            con_num = len(contents.find_elements_by_xpath("//ytd-video-renderer"))
+
+            if con_num >= 100:
+                print(con_num)
+                break
+            else:
+                self.__down(body)
+            print(con_num)
+
+    def __do(self):
+        pass
 
     def run(self):
-        data = {"context": {
-            "client": {"hl": "zh-CN", "gl": "US", "remoteHost": "223.16.18.167", "deviceMake": "", "deviceModel": "",
-                       "visitorData": "CgtxSHdqdHBDd3N5RSixjKGGBg%3D%3D",
-                       "userAgent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36,gzip(gfe)",
-                       "clientName": "WEB", "clientVersion": "2.20210613.07.00", "osName": "Windows",
-                       "osVersion": "10.0", "originalUrl": "https://www.youtube.com/results?search_query=light",
-                       "platform": "DESKTOP", "clientFormFactor": "UNKNOWN_FORM_FACTOR", "timeZone": "America/New_York",
-                       "browserName": "Chrome", "browserVersion": "91.0.4472.77", "screenWidthPoints": 939,
-                       "screenHeightPoints": 937, "screenPixelDensity": 1, "screenDensityFloat": 1,
-                       "utcOffsetMinutes": -240, "userInterfaceTheme": "USER_INTERFACE_THEME_LIGHT",
-                       "mainAppWebInfo": {"graftUrl": "/results?search_query=light",
-                                          "webDisplayMode": "WEB_DISPLAY_MODE_BROWSER",
-                                          "isWebNativeShareAvailable": True}}, "user": {"lockedSafetyMode": False},
-            "request": {"useSsl": True, "internalExperimentFlags": [], "consistencyTokenJars": []},
-            "clickTracking": {"clickTrackingParams": "CBcQ7VAiEwiOkqzEsJnxAhXEYCoKHWj5Cfs="}, "adSignalsInfo": {
-                "params": [{"key": "dt", "value": "1623737905524"}, {"key": "flash", "value": "0"},
-                           {"key": "frm", "value": "0"}, {"key": "u_tz", "value": "-240"},
-                           {"key": "u_his", "value": "4"}, {"key": "u_java", "value": "false"},
-                           {"key": "u_h", "value": "1080"}, {"key": "u_w", "value": "1920"},
-                           {"key": "u_ah", "value": "1040"}, {"key": "u_aw", "value": "1920"},
-                           {"key": "u_cd", "value": "24"}, {"key": "u_nplug", "value": "3"},
-                           {"key": "u_nmime", "value": "4"}, {"key": "bc", "value": "31"},
-                           {"key": "bih", "value": "937"}, {"key": "biw", "value": "923"},
-                           {"key": "brdim", "value": "0,0,0,0,1920,0,1920,1040,939,937"}, {"key": "vis", "value": "1"},
-                           {"key": "wgl", "value": "true"}, {"key": "ca_type", "value": "image"}]}}, "query": "light",
-                "webSearchboxStatsUrl": "/search?oq=light&gs_l=youtube.12...0.0.2.2902812.0.0.0.0.0.0.0.0..0.0....0...1ac..64.youtube..0.0.0....0.Ycsp_Yt-Q_g"}
-        r = requests.post(url=self.__url, data=data, headers=self.__headers, proxies=self.__proxies)
-        print(r.json())
+        self.__open_browser()
