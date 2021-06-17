0d4d07173556bd7a69bdb0e3b01086fb567dc0c6
zymousai <zymousai@163.com>
2021-06-18 00:02:51 +0800

    需求二完成90……%

--- core/spider.py
+++ core/spider.py
@@ -48,6 +48,12 @@ class YSpider(object):
 
         self.__driver = None
 
+        # 每次运行的唯一标识
+        self.uuid_str = str(uuid.uuid1())
+
+        # 总共需要多少条数据
+        self.__need_data_num = 20
+
     def __create_opt(self):
         self.__opt = webdriver.ChromeOptions()
 
@@ -75,8 +81,8 @@ class YSpider(object):
         return driver
 
     @staticmethod
-    def __down(body):
-        for x in range(20):
+    def __down(body, num):
+        for x in range(num):
             body.send_keys(Keys.PAGE_DOWN)
             time.sleep(random.uniform(0.3, 0.5))
 
@@ -91,16 +97,17 @@ class YSpider(object):
         while True:
             con_num = len(self.driver.find_elements_by_xpath("//ytd-video-renderer"))
 
-            if con_num >= 20:
+            if con_num >= self.__need_data_num:
                 break
             else:
-                self.__down(body)
+                self.__down(body, 20)
 
     def __do_one(self):
         # 1.需求一
         print('=========开始处理需求一')
         contents_els = self.driver.find_elements_by_xpath(
-            "//ytd-video-renderer//div[@id='dismissible']/div/div[@id='channel-info']/ytd-channel-name")[:1000]
+            "//ytd-video-renderer//div[@id='dismissible']/div/div[@id='channel-info']/ytd-channel-name")[
+                       :self.__need_data_num]
         # 建立处理csv所需数据
         now_day = datetime.datetime.now().strftime("%Y-%m-%d")
         df1 = pd.DataFrame(columns=("抓取日期", "关键词", "作者名称", "认证状态", "作者频道链接"))
@@ -123,7 +130,7 @@ class YSpider(object):
             author_tag = 1 if auth_tag_el else 0
 
             if author_link not in de_duplication_list:
-                # 处理第二部需要用到得数据
+                # 处理第二步需要用到得数据
                 self.duplication_list.append({
                     "author": author,
                     "author_link": author_link,
@@ -138,10 +145,8 @@ class YSpider(object):
                 de_duplication_list.append(author_link)
         print(f'=========去重后数据还剩{len(de_duplication_list)}条数据')
         # 1.1 写入csv
-        # 每次运行的唯一标识
-        uuid_str = str(uuid.uuid1())
         # 拼接csv名字
-        csv_name = now_day + "&" + self.keyword + "&" + 'one' + '&' + uuid_str + ".csv"
+        csv_name = now_day + "&" + self.keyword + "&" + 'one' + '&' + self.uuid_str + ".csv"
         # 拼接csv路径
         csv_path = f"./result/{csv_name}"
         df1.to_csv(csv_path, encoding='utf_8_sig')
@@ -151,29 +156,113 @@ class YSpider(object):
 
     def do_two(self):
         """
-        抓取日期 | 关键词 | 作者名称 | 认证状态 | 粉丝数量 | 作者频道链接 | 2个月内视频观看总数 | 2个月内视频发布总数 | 2个月内视频平均观看数 | 说明 | 商务咨询 | 位置 | 注册时间 | 观看总次数
+        处理需求二
+        :return:
         """
+        print('=========开始处理需求二')
+        # todo 模拟数据
         wait_info = [
             {
-                'author': 1,
+                'author': 'NairoMK',
                 'author_link': 'https://www.youtube.com/channel/UCTSCjjnCuAPHcfQWNNvULTw',
                 'keyword': 'light',
                 'auth_tag': 0
             }
         ]
+        # 2.需求二
         self.__create_opt()
+
+        now_day = datetime.datetime.now().strftime("%Y-%m-%d")
+        df2 = pd.DataFrame(columns=("抓取日期", "关键词", "作者名称", "认证状态", "粉丝数量", "作者频道链接",
+                                    "2个月内视频观看总数", "2个月内视频发布总数", "2个月内视频平均观看数",
+                                    "说明", "商务咨询", "位置", "注册时间", "观看总数"))
         for index, info in enumerate(wait_info):
+            print(f'=========需求二：开始处理第{index + 1}条数据')
             # 打开链接
             driver = self.__create_driver()
             driver.get(info['author_link'])
-
             # 判断视频日期
-            body = self.driver.find_element_by_tag_name('body')
-
-            # todo
-
+            body = driver.find_element_by_tag_name('body')
+            # 点击切换到视频标签
             driver.find_element_by_xpath('//*[@id="tabsContent"]/tp-yt-paper-tab[2]/div').click()
             time.sleep(3)
+
+            while True:
+                date_els = driver.find_elements_by_xpath("//*[@id='metadata-line']/span[2]")
+                date_text_list = [date_el.text for date_el in date_els]
+                if '2个月前' in date_text_list:
+                    break
+                else:
+                    self.__down(body, 5)
+
+            time.sleep(5)
+            # 获取两个月内得视频个数
+            videos_el_list = []
+            videos_els = driver.find_elements_by_xpath('//ytd-grid-video-renderer')
+            for videos_el in videos_els:
+                videos_date = videos_el.find_element_by_xpath(".//*[@id='metadata-line']/span[2]").text
+                if '年' in videos_date:
+                    break
+                elif '月' in videos_date:
+                    if '1个月前' == videos_date:
+                        videos_el_list.append(videos_el)
+                elif '小时' in videos_date or '天' in videos_date or '周' in videos_date:
+                    videos_el_list.append(videos_el)
+                else:
+                    pass
+
+            total_play_nums = 0
+            # 获取两个月内视频播放总数
+            for v_el in videos_el_list:
+                play_nums = v_el.find_element_by_xpath(".//*[@id='metadata-line']/span[1]").text
+                if '万' in play_nums:
+                    curr_play_nums = float(play_nums.split('万')[0]) * 10000
+                    total_play_nums += curr_play_nums
+                else:
+                    total_play_nums += float(play_nums.split('次')[0])
+
+            # 两个月内平均观看数量
+            mid_play_nums = total_play_nums / 2
+            # 粉丝数量
+            fans_num = driver.find_element_by_xpath('//*[@id="subscriber-count"]').text
+
+            # 切换到简介
+            driver.find_element_by_xpath('//*[@id="tabsContent"]/tp-yt-paper-tab[6]/div').click()
+            time.sleep(3)
+            # 说明
+            description = driver.find_element_by_xpath('//*[@id="description"]').text
+            # 位置
+            address_els = driver.find_elements_by_xpath(
+                '//*[@id="details-container"]/table/tbody/tr[2]/td[2]/yt-formatted-string')
+            if address_els:
+                address = address_els[0].text
+            else:
+                address = None
+            # 注册时间
+            si_date = driver.find_element_by_xpath('//*[@id="right-column"]/yt-formatted-string[2]/span[1]').text
+            # 观看总数
+            all_total_play_nums = driver.find_element_by_xpath('//*[@id="right-column"]/yt-formatted-string[3]').text
+            # todo 商务咨询，需要过谷歌验证码
+            b_aff = None
+
+            """
+                    df2 = pd.DataFrame(columns=("抓取日期", "关键词", "作者名称", "认证状态", "粉丝数量", "作者频道链接",
+                                    "2个月内视频观看总数", "2个月内视频发布总数", "2个月内视频平均观看数",
+                                    "说明", "商务咨询", "位置", "注册时间", "观看总数"))
+            """
+            # 完善df2
+            df2.loc[index] = [now_day, self.keyword, info['author'], info['auth_tag'], fans_num, info['author_link'],
+                              total_play_nums, len(videos_el_list), mid_play_nums, description, b_aff, address, si_date,
+                              all_total_play_nums]
+            print(f'=========需求二，第{index + 1}条数据处理完毕')
+
+        # 2.1需求二写入
+        # 拼接csv名字
+        csv_name = now_day + "&" + self.keyword + "&" + 'two' + '&' + self.uuid_str + ".csv"
+        # 拼接csv路径
+        csv_path = f"./result/{csv_name}"
+        df2.to_csv(csv_path, encoding='utf_8_sig')
+        print(f'=========需求一完成，已写入文件，|文件名{csv_name}|')
 
     def __close_driver(self):
         self.driver.close()
@@ -187,5 +276,5 @@ class YSpider(object):
         self.do_two()
 
 
-ax = YSpider('123')
+ax = YSpider('light')
 ax.do_two()
